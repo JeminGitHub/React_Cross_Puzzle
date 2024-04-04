16128b455315e546e165fee618f7f55243c0b032
unknown <jeminmthw997@gmail.com>
2024-04-04 13:36:17 +0530

    puzzle first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Puzzlez from './components/pages/Puzzlez';
+
+function App() {
+  return (
+    <div className="App">
+    <Puzzlez/>
+
+    </div>
+  );
+}
+
+export default App;
